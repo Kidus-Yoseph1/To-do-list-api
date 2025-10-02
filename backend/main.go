53ab65344d25ee8154f37fd8f672d729698a7e74
Kidus yoseph <kidusy567@gmail.com>
2025-10-02 16:19:33 +0300

    CRUP API for todo list

---
+++ backend/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Todo struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var lists = []Todo{
+	{ID: 1, Title: "Assignment", Completed: false, CreatedAt: time.Now()},
+	{ID: 2, Title: "Practice GO", Completed: true, CreatedAt: time.Now()},
+}
+
+func getTodo(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, lists)
+}
+
+func creatTodo(c *gin.Context) {
+	var newTodo Todo
+	if err := c.ShouldBindJSON(&newTodo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	if strings.TrimSpace(newTodo.Title) == " " {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "title can not be empty"})
+	}
+	newTodo.ID = len(lists) + 1
+	lists = append(lists, newTodo)
+	c.IndentedJSON(http.StatusCreated, newTodo)
+}
+
+func updateTodo(c *gin.Context) {
+	id_param := c.Param("id")
+	id, _ := strconv.Atoi(id_param)
+	for i, todo := range lists {
+		if todo.ID == id {
+			lists[i].Completed = true
+			c.JSON(http.StatusOK, lists[i])
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"error": "todo not found"})
+}
+
+func main() {
+	r := gin.Default()
+	api := r.Group("/api")
+	{
+		api.GET("/lists", getTodo)
+		api.POST("/lists", creatTodo)
+		api.PATCH("/update/:id", updateTodo)
+
+	}
+	r.Run("localhost:8080")
+}
